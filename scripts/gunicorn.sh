bf9c53f5ad8dfca0886d54bdccab26d2e546eb72
rajeev <rajeevkl07@gmail.com>
2025-07-29 18:52:11 +0530

    fix: Update Gunicorn binding address to allow external access

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -3,7 +3,7 @@ source /home/ubuntu/.bashrc
 CPU_COUNT=$(nproc)
 WORKER_COUNT=$(($CPU_COUNT * 3 + 1))
 /home/ubuntu/.local/bin/pipenv run gunicorn conf.wsgi:application \
-    --bind=127.0.0.1:7100 \
+    --bind=0.0.0.0:7100 \
     --workers=$WORKER_COUNT \
     --threads=4 \
     --timeout=20
